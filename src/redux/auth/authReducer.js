5db4aa888960842d033243d7ea4e94618c59004a
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-05-19 17:25:06 -0400

    Minor reformatting

--- src/redux/auth/authReducer.js
+++ src/redux/auth/authReducer.js
@@ -1,26 +1,26 @@
 const INITIAL_STATE = {
-     authError: null
-}
+  authError: null,
+};
 
 const authReducer = (state = INITIAL_STATE, action) => {
-     switch(action.type){
-          case "LOGIN_SUCCESS":
-               console.log("login success")
-               return {
-                    ...state,
-                    authError: null
-               }
-          case "LOGIN_FAILURE":
-               console.log("login failure")
-               return {
-               ...state,
-               authError: "Login failed"
-               }
-               default: 
-               return state
-     }
+  switch (action.type) {
+    case "LOGIN_SUCCESS":
+      console.log("login success");
+      return {
+        ...state,
+        authError: null,
+      };
+    case "LOGIN_FAILURE":
+      console.log("login failure");
+      return {
+        ...state,
+        authError: "Login failed",
+      };
+    default:
+      return state;
+  }
 
-     return state
-}
+  return state;
+};
 
-export default authReducer
+export default authReducer;
