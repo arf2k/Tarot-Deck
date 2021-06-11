a9e68b9b945fd9b3bb664877f373aab35b9e3d7e
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-06-11 14:10:42 -0400

    Cleanup comments and formatting

--- src/pages/readings/ReadingsTriple.js
+++ src/pages/readings/ReadingsTriple.js
@@ -2,12 +2,12 @@ import React from "react";
 import SavedThree from "../../component/saved-cards/SavedThree";
 
 const ReadingsTriple = () => {
-     return(
-          <>
-          <h1>Saved Readings</h1>
-          <SavedThree/>
-          </>
-     )
-}
+  return (
+    <>
+      <h1>Saved Readings</h1>
+      <SavedThree />
+    </>
+  );
+};
 
-export default ReadingsTriple
+export default ReadingsTriple;
